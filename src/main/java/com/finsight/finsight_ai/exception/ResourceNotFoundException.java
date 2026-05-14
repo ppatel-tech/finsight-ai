51e46fe3f5a4609b1f4ef6c4e7589f6e0673e93c
ppatel-tech <purushottampatel.tech@gmail.com>
2026-05-14 10:49:47 +0530

    added ResourceNotFoundException for exception handling

---
+++ src/main/java/com/finsight/finsight_ai/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.finsight.finsight_ai.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+
+        super(message);
+    }
+
+}
