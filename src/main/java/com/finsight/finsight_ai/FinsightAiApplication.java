3c1facabcaed97222cc48aba3974504e9860c644
ppatel-tech <purushottampatel.tech@gmail.com>
2026-05-19 14:40:44 +0530

    enabled scheduling

--- src/main/java/com/finsight/finsight_ai/FinsightAiApplication.java
+++ src/main/java/com/finsight/finsight_ai/FinsightAiApplication.java
@@ -2,8 +2,10 @@ package com.finsight.finsight_ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FinsightAiApplication {
 
 	public static void main(String[] args) {
