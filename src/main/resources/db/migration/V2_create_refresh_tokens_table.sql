312a6453369f768543d1739fed09974d8703e92a
ppatel-tech <purushottampatel.tech@gmail.com>
2026-05-22 10:33:25 +0530

    created sql script for refresh token  table

---
+++ src/main/resources/db/migration/V2_create_refresh_tokens_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS refresh_tokens(
+    id BIGSERIAL PRIMARY KEY,
+    token VARCHAR(255) NOT NULL UNIQUE,
+    expiry_date TIMESTAMP NOT NULL,
+    user_id BIGINT UNIQUE,
+    CONSTRAINT fk_refresh_token_user
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+)
